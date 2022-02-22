5d12803891a052ccde46caf7befd7d719917b151
Vijendra <docs.vijendra@gmail.com>
2022-02-22 19:07:15 +0530

    moved views and public folder at their right position

---
+++ public/js/header.js
@@ -0,0 +1,155 @@
+const navbar = ()=>{
+    return `
+    <div id="black_header">
+    <div id="Black_footer_main">
+        <ul>
+            <li><a href="./levihelp.html">HELP</a></li>
+            <li><a href="">TRACK ORDER</a></li>
+            <li><a href="./store_locator.html">STORE LOCATOR</a></li>
+            <li><a href="./login.html">SIGN IN</a></li>
+            <li><img src="https://media.istockphoto.com/vectors/flag-of-india-vector-id472317739?b=1&k=20&m=472317739&s=170667a&w=0&h=Ju9wPp7N7fM00TiZtVuVr_wc6elG1L-b32DMlY7JHc4=" alt=""></li>
+        </ul>
+    </div>
+</div>
+<div class="navigation-bar">
+    <div class="logo-image">
+        <a href="./index.html">
+            <img src="./IMAGES/logo.jpg"
+            alt="">
+        </a>
+    </div>
+    <div>
+        <ul class="category-name">
+            <li> <span><a href="./menProducts.html">MEN</a></span>
+                <div class="category-container">
+                    <ul>
+                        <h5>CLOTHING</h5>
+                        <li><a href="./menProducts.html"> Jeans</a></li>
+                        <li><a href="./menProducts.html"> Pants</a></li>
+                        <li><a href="./menProducts.html"> Shorts</a></li>
+                        <li><a href="./menProducts.html"> T-Shirts & Polos</a></li>
+                        <li><a href="./menProducts.html"> Shirts</a></li>
+                        <li><a href="./menProducts.html"> Jackets</a></li>
+                        <li><a href="./menProducts.html"> Sweaters & Sweatshirts</a></li>
+                    </ul>
+
+                    <ul>
+                        <h5>SHOP JEANS BY FIT</h5>
+                        <li><a href="./menProducts.html"> Skinny</a></li>
+                        <li><a href="./menProducts.html"> Slim</a></li>
+                        <li><a href="./menProducts.html"> Tapered</a></li>
+                        <li><a href="./menProducts.html"> Straight</a></li>
+                        <li><a href="./menProducts.html"> Relaxed</a></li>
+                    </ul>
+
+                    <ul>
+                        <h5>ACCESSORIES</h5>
+                        <li><a href="#"> Belts</a></li>
+                        <li><a href="#"> Wallets</a></li>
+                        <li><a href="#"> Bags & Backpacks</a></li>
+                        <li><a href="#"> Caps</a></li>
+                        <li><a href="#"> Footwear</a></li>
+                    </ul>
+
+                    <ul>
+                        <h5>COLLECTIONS</h5>
+                        <li><a href="#"> Sustainability</a></li>
+                        <li><a href="#"> Levi's® X Super Mario™</a></li>
+                        <li><a href="#"> RedLoop™</a></li>
+                        <li><a href="#"> White Tab</a></li>
+                        <li><a href="#"> Winterwear</a></li>
+                        <li><a href="#"> Levi's® Flex</a></li>
+                    </ul>
+
+                </div>
+            </li>
+            <li> <span><a href="./women.html">WOMEN</a></span>
+                <div class="category-container">
+                    <ul>
+                        <h5>CLOTHING</h5>
+                        <li><a href="./women.html">Jeans</a></li>
+                        <li><a href="./women.html">T-Shirts & Tops</a></li>
+                        <li><a href="./women.html">Shirts</a></li>
+                        <li><a href="./women.html">Jackets</a></li>
+                        <li><a href="./women.html">Shirts</a></li>
+                        <li><a href="./women.html">Shorts & Skirts</a></li>
+                        <li><a href="./women.html">Pants</a></li>
+                        <li><a href="./women.html">Sweaters & Sweatshirts</a></li>
+                    </ul>
+                    <ul>
+                        <h5>SHOP JEANS BY FIT</h5>
+                        <li><a href="./women.html">Super Skinny</a></li>
+                        <li><a href="./women.html">Skinny</a></li>
+                        <li><a href="./women.html">Slim</a></li>
+                        <li><a href="./women.html">Balloon Leg</a></li>
+                        <li><a href="./women.html">High Rise</a></li>
+
+                    </ul>
+                    <ul>
+                        <h5>ACCESSORIES</h5>
+                        <li><a href="#">Bags & Backpacks</a></li>
+                        <li><a href="#">Belts</a></li>
+                        <li><a href="#">Sneakers</a></li>
+                        <li><a href="#">Footwear</a></li>
+                    </ul>
+                    <ul>
+                        <h5>COLLECTIONS</h5>
+                        <li><a href="#">Sustainability</a></li>
+                        <li><a href="#">Levi's® X Super Mario™</a></li>
+                        <li><a href="#">Balloon Jeans</a></li>
+                        <li><a href="#">Sculpt Jeans</a></li>
+                        <li><a href="#">Levi's® Made & Crafted®</a></li>
+                        <li><a href="#"></a>Winterwear</li>
+                        <li><a href="#">RedLoop™</a></li>
+                        <li><a href="#">White Tab</a></li>
+                    </ul>
+                </div>
+            </li>
+            <li><span>NEW ARRIVALS</span>
+                <div class="category-container">
+                    <ul>
+                        <li><a href="#">Men</a></li>
+                        <li><a href="#">Women</a></li>
+                        <li><a href="#">Online Exclusive</a></li>
+                    </ul>
+                </div>
+            </li>
+            <li><span>COLLECTIONS</span>
+                <div class="category-container">
+                    <ul>
+                        <li><a href="">REDLOOP</a></li>
+                        <li><a href="">LEVI'S® MADE & CRAFTED®</a></li>
+                        <li><a href="">FASHIONFITS</a></li>
+                        <li><a href="">LEVI'S X DEEPIKA PADUKONE</a></li>
+                        <li><a href="">WINTERWEAR COLLECTION</a></li>
+                    </ul>
+                </div>
+            </li>
+            <li><span>SALE</span>
+                <div class="category-container">
+                    <ul>
+                        <li><a href="">Men</a></li>
+                        <li><a href="">Women</a></li>
+                        <li><a href="">FLAT 60%</a></li>
+                    </ul>
+                </div>
+            </li>
+            <li><span>WINTERWEAR COLLECTION</span>
+
+            </li>
+        </ul>
+    </div>
+    <div>
+        <div class="nav-search-box">
+            <input type="text" placeholder="Search">
+            <span><i class="fas fa-search"></i></span>
+        </div>
+    </div>
+    <div class="cart-icon-in-header">
+    <a href="./cart.html"><span id='cartIcon'><i class="fas fa-shopping-cart"></i></span></a> 
+    </div>
+    </div>
+    `
+}
+
+    export default navbar
--- src/server.js
+++ src/server.js
@@ -1,15 +1,30 @@
 const express = require('express');
 const mongodbConnect = require('./configs/db');
+const path = require('path')
+
+
+const homeController = require('./controllers/home.controller')
+const registerAndLoginController = require('./controllers/registerandlogin.controller')
+
+
+
 const app = express();
 app.use(express.json());
+
+app.use(express.urlencoded({ extended : true}))
+app.use(express.static('public'))
+app.set('view engine','ejs')   
+
+app.use('/home', homeController)
+app.use('/login', registerAndLoginController)
 
 
 
 module.exports = ()=>{
-    app.listen( 6666, async () => {
+    app.listen( 7000, async () => {
         try {
-            await mongodbConnect()
-            console.log("Server is running on the port 6666")
+            // await mongodbConnect()
+            console.log("Server is running on the port 7000")
         } catch (error) {
             console.log({
                 status : "connection failed",
