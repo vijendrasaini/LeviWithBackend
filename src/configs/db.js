1f6324f8f12989ef63783b4e174bbc8dd6f6e35d
tanishaa17 <96083845+tanishaa17@users.noreply.github.com>
2022-02-22 15:22:47 +0530

    Revert "Test"

--- src/configs/db.js
+++
@@ -1,7 +0,0 @@
-const mongoose = require('mongoose')
-const dotenv = require('dotenv')
-dotenv.config()
-
-const db = `mongodb+srv://teamlevis:${process.env.MONGOATLAS_SECURITY_KEY}@cluster0.9xxdh.mongodb.net/Levis?retryWrites=true&w=majority`
-
-module.exports = () => mongoose.connect(db)
