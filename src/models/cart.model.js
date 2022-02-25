1176c5479aa1c352ca49764a593ece3df22c37fb
Vijendra <docs.vijendra@gmail.com>
2022-02-25 15:25:59 +0530

    changes in cart model

--- src/models/cart.model.js
+++ src/models/cart.model.js
@@ -3,12 +3,13 @@ const cartSchema = new mongoose.Schema(
     {
         email : { type : String, required : true },
         
+        image : { type : String, required : true },
         product_id : { type : String, required : true },
         title : { type : String, required : true },
         color : { type : String, required : true, default : "Green" },
-        size : { type : Number, required : true, default : 1 },
-        qty : { type : Number, required : true, default : 1 },
-        price : { type : Number, required : true }
+        size : { type : String, required : true, default : 1 },
+        qty : { type : String, required : true, default : 1 },
+        price : { type : String, required : true }
     }, 
     {
         versionKey: false
