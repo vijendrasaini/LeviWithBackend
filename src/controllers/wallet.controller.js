09e4de1b850c9eab291de08d2afc107fd95b7190
tanishaa17 <krati1701@gmail.com>
2022-02-26 14:59:59 +0530

    errors fixed

---
+++ src/controllers/wallet.controller.js
@@ -0,0 +1,18 @@
+const { Router } = require('express')
+const router = Router()
+
+router.get('', async ( req, res)=>{
+    try {
+        return res
+        .render("ejs/walle.ejs")
+    } catch (error) {
+        return res
+        .status(500)
+        .send({
+            status : "get method error in wallet.controller",
+            message : error.message
+        })
+    }
+})
+
+module.exports = router
