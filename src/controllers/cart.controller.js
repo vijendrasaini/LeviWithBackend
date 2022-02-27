b58b225d2f56b48f06df4ffa7ef39f8d2dc263b4
Vijendra <docs.vijendra@gmail.com>
2022-02-27 09:19:29 +0530

    product removing from cart functionality added.

--- src/controllers/cart.controller.js
+++ src/controllers/cart.controller.js
@@ -4,6 +4,11 @@ const Cart = require('../models/cart.model')
 
 router.get( '/:id',async ( req, res )=>{
     try {
+        const deleted_product_id = req.query.id 
+        if(req.query.id)
+        {
+            let deleted_product = await Cart.findByIdAndDelete(deleted_product_id).lean().exec()
+        }
         const products = await Cart.find({user_id : req.params.id}).lean().exec()
         return res
         .render("ejs/newcart.ejs", {products})
@@ -18,17 +23,35 @@ router.get( '/:id',async ( req, res )=>{
 })
 router.post( '',async ( req, res )=>{
     try {
-        let product = await Cart.findOne({ email : req.body.email, product_id : req.body.product_id}).lean().exec()
+        let product = await Cart.findOne({ user_id : req.body.user_id, product_id : req.body.product_id}).lean().exec()
+        
         if(product)
         {
-            product.quantity++
-            product = await Cart.findByIdAndUpdate(product._id,product).lean().exec()
+            product.qty++
+            product = await Cart.findByIdAndUpdate(product._id,product,{ new : true}).lean().exec()
             return res
             .send({ qyt : product.quantity})
         }
         product = await Cart.create(req.body)
         return res
         .send({ qyt : product.quantity})
+    } catch (error) {
+        return res
+        .status(500)
+        .send({
+            status : "Error from /cart route",
+            message : error.message
+        })
+    }
+})
+
+
+router.get( '/:id',async ( req, res )=>{
+    try {
+        const products = await Cart.find({user_id : req.params.id}).lean().exec()
+        console.log(products)
+        return res
+        .render("ejs/newcart.ejs", {products})
     } catch (error) {
         return res
         .status(500)
