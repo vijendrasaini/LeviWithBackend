943a50012adaf4d98c5e64749cda05791db9a630
Vijendra <docs.vijendra@gmail.com>
2022-03-12 17:24:12 +0530

    callbackUrl is changed in passport.js

--- src/configs/passport.js
+++ src/configs/passport.js
@@ -7,7 +7,7 @@ const User = require('../models/user.model')
 passport.use(new GoogleStrategy({
     clientID:     process.env.GOOGLE_CLIENT_ID,
     clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    callbackURL: "http://localhost:7000/oauth/signin/callback",
+    callbackURL: "/oauth/signin/callback",
     passReqToCallback   : true
   },
   async function(request, accessToken, refreshToken, profile, done) {
